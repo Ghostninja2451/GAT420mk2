e4697ec3543c82a8a822e763698b0f807472ad5b
Ghostninja2451 <80051664+Ghostninja2451@users.noreply.github.com>
2022-01-14 08:24:33 -0700

    Cohesion, Alignment, separation

--- Assets/Script/AutonomusAgent.cs
+++ Assets/Script/AutonomusAgent.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class AutonomusAgent : Agent
 {
     [SerializeField] Perception perception;
+    [SerializeField] Perception flockperception;
     [SerializeField] Steering steering;
+    [SerializeField] AutomonousAgentData agentData;
 
-    public float maxSpeed;
-    public float maxForce;
+    public float maxSpeed { get { return agentData.maxSpeed; } }
+    public float maxForce { get { return agentData.maxForce; } }
 
     public Vector3 velocity { get; set; } = Vector3.zero;
 
@@ -18,20 +20,30 @@ public class AutonomusAgent : Agent
         Vector3 acceleration = Vector3.zero;
 
         GameObject[] gameObjects = perception.GetGameObjects();
+
+        //seek / flee
         if(gameObjects.Length !=0 )
         {
-            Debug.DrawLine(transform.position, gameObjects[0].transform.position);
 
             //Vector3 force = gameObjects[0].transform.position - transform.position;
-            Vector3 force = steering.Seek(this, gameObjects[0]);
-            acceleration += force;
+            acceleration = steering.Seek(this, gameObjects[0]) * agentData.seekWeight;
+            acceleration = steering.Flee(this, gameObjects[0]) * agentData.fleeWeight;
+            
 
         }
         else
         {
             acceleration += steering.Wander(this);
         }
-
+        //flocking
+        gameObjects = flockperception.GetGameObjects();
+        if(gameObjects.Length != 0)
+        {
+            acceleration += steering.Cohesion(this, gameObjects) * agentData.cohesionWeight;
+            acceleration += steering.Seperation(this, gameObjects, agentData.separationRadius) * agentData.separationWeight;
+            acceleration += steering.Alignment(this, gameObjects) * agentData.alignmentWeight;
+        }
+        
         velocity += acceleration * Time.deltaTime;
         velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
         transform.position += velocity * Time.deltaTime;
--- Assets/Script/Steering.cs
+++ Assets/Script/Steering.cs
@@ -36,6 +36,46 @@ public class Steering : MonoBehaviour
         
     }
 
+    public Vector3 Cohesion(AutonomusAgent agent, GameObject[] targets)
+    {
+        Vector3 centerOfTargets = Vector3.zero;
+        foreach(GameObject target in targets)
+        {
+            centerOfTargets += target.transform.position;
+        }
+
+        centerOfTargets /= targets.Length;
+
+        Vector3 force = CalculateSteering(agent, centerOfTargets - agent.transform.position);
+
+        return force;
+    }
+    public Vector3 Seperation(AutonomusAgent agent, GameObject[] targets, float radius)
+    {
+        Vector3 separation = Vector3.zero;
+        foreach(GameObject target in targets)
+        {
+            Vector3 direction = (agent.transform.position - target.transform.position);
+            if(direction.magnitude < radius)
+            {
+                separation += direction / direction.sqrMagnitude;
+            }
+        }
+        Vector3 force = CalculateSteering(agent, separation);
+        return force;
+    }
+    public Vector3 Alignment(AutonomusAgent agent, GameObject[] targets)
+    {
+        Vector3 averageVelocity = Vector3.zero;
+        foreach (GameObject target in targets)
+        {
+            averageVelocity += target.GetComponent<AutonomusAgent>().velocity;
+        }
+        averageVelocity /= targets.Length;
+        Vector3 force = CalculateSteering(agent, averageVelocity);
+        return force;
+    }
+
     Vector3 CalculateSteering(AutonomusAgent agent, Vector3 vector)
     {
         Vector3 direction = vector.normalized;
